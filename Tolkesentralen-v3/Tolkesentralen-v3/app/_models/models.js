7464725988a8aae366ee3fa2b50e4be0dee243d4
lunga <s929577@stud.hioa.no>
2017-03-23 11:17:39 +0100

    My addings

---
+++ Tolkesentralen-v3/Tolkesentralen-v3/app/_models/models.js
@@ -0,0 +1,71 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var NavbarElement = (function () {
+    function NavbarElement() {
+    }
+    return NavbarElement;
+}());
+exports.NavbarElement = NavbarElement;
+var Person = (function () {
+    function Person() {
+    }
+    return Person;
+}());
+exports.Person = Person;
+var Oppdrag = (function () {
+    function Oppdrag() {
+    }
+    return Oppdrag;
+}());
+exports.Oppdrag = Oppdrag;
+var OppdragForRegistrert = (function () {
+    function OppdragForRegistrert() {
+    }
+    return OppdragForRegistrert;
+}());
+exports.OppdragForRegistrert = OppdragForRegistrert;
+var Tolk = (function () {
+    function Tolk() {
+    }
+    return Tolk;
+}());
+exports.Tolk = Tolk;
+var Login = (function () {
+    function Login() {
+    }
+    return Login;
+}());
+exports.Login = Login;
+var Oversettelse = (function () {
+    function Oversettelse() {
+    }
+    return Oversettelse;
+}());
+exports.Oversettelse = Oversettelse;
+var Kunde = (function (_super) {
+    __extends(Kunde, _super);
+    function Kunde() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Kunde;
+}(Person));
+exports.Kunde = Kunde;
+var Hero = (function () {
+    function Hero(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=models.js.map
---
+++ Tolkesentralen-v3/Tolkesentralen-v3/app/_services/oppdrag.service.js
@@ -0,0 +1,53 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
+require("rxjs/add/operator/map");
+var auth_service_1 = require("../_services/auth.service");
+var OppdragService = (function () {
+    function OppdragService(http, authenticationService) {
+        this.http = http;
+        this.authenticationService = authenticationService;
+        this.url = 'api/oppdrag'; // URL to web API
+    }
+    OppdragService.prototype.postOversettelse = function (body) {
+        var headers = new http_1.Headers({ "Content-Type": "application/json" });
+        return this.http.post(this.url, body, { headers: headers })
+            .map(function (returData) { return returData.toString(); });
+    };
+    //postOppdrag(body: any, id: number) {
+    //    var headers = new Headers({ "Content-Type": "application/json" });
+    //    return this.http.post(this.url+"/"+id, body, { headers: headers })
+    //        .map(returData => returData.toString())
+    //}
+    OppdragService.prototype.postOppdrag = function (body) {
+        var headers = new http_1.Headers({ "Content-Type": "application/json" });
+        return this.http.post(this.url, body, { headers: headers })
+            .map(function (returData) { return returData.toString(); });
+    };
+    OppdragService.prototype.getOversettelser = function () {
+        return this.http.get(this.url)
+            .map(function (response) { return response.json(); });
+    };
+    OppdragService.prototype.getOppdrag = function () {
+        return this.http.get(this.url)
+            .map(function (response) { return response.json(); });
+    };
+    return OppdragService;
+}());
+OppdragService = __decorate([
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [http_1.Http,
+        auth_service_1.AuthenticationService])
+], OppdragService);
+exports.OppdragService = OppdragService;
+//# sourceMappingURL=oppdrag.service.js.map
