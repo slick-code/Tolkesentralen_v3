8155b62532d1321ecfbe833595676e28cf111836
lunga <s929577@stud.hioa.no>
2017-03-24 12:49:50 +0100

    add

--- Tolkesentralen-v3/Tolkesentralen-v3/Controllers/OppdragController.cs
+++ Tolkesentralen-v3/Tolkesentralen-v3/Controllers/OppdragController.cs
@@ -161,27 +161,6 @@ namespace Tolkesentralen_v3.Controllers
             };
         }
 
-        //[System.Web.Mvc.HttpPost]
-        //public HttpResponseMessage Post([FromBody]Kunde_vm ny)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        bool OK = repository.settInnKunde(ny);
-
-        //        if (OK)
-        //        {
-        //            return new HttpResponseMessage()
-        //            {
-        //                StatusCode = HttpStatusCode.OK
-        //            };
-
-        //        }
-        //    }
-        //    return new HttpResponseMessage()
-        //    {
-        //        StatusCode = HttpStatusCode.BadRequest,
-        //        Content = new StringContent("Søknaden ble ikke lagret!")
-        //    };
-        //}
+     
     }
 }
