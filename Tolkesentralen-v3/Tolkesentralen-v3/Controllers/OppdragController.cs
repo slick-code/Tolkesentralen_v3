df76510e75fa7f625233d3b75477e51aec8eb4a8
Patrick <spnm_88@hotmail.com>
2017-04-18 14:36:47 +0200

    testM

--- Tolkesentralen-v3/Tolkesentralen-v3/Controllers/OppdragController.cs
+++ Tolkesentralen-v3/Tolkesentralen-v3/Controllers/OppdragController.cs
@@ -79,21 +79,21 @@ namespace Tolkesentralen_v3.Controllers
             };
         }
 
-        [Route("api/oppdrag/GetForesposlerSendt")]
-        public HttpResponseMessage GetForesposlerSendt()
-        {
-            DbForessporsel f = new DbForessporsel();
-            List<Tolking_vm> liste = f.listTolkForesporslerMedID();
+        //[Route("api/oppdrag/GetForesposlerSendt")]
+        //public HttpResponseMessage GetForesposlerSendt()
+        //{
+        //    DbForessporsel f = new DbForessporsel();
+        //    List<Tolking_vm> liste = f.listTolkForesporslerMedID();
             
-            var Json = new JavaScriptSerializer();
-            string JsonString = Json.Serialize(liste);
+        //    var Json = new JavaScriptSerializer();
+        //    string JsonString = Json.Serialize(liste);
 
-            return new HttpResponseMessage()
-            {
-                Content = new StringContent(JsonString, Encoding.UTF8, "application/json"),
-                StatusCode = HttpStatusCode.OK
-            };
-        }
+        //    return new HttpResponseMessage()
+        //    {
+        //        Content = new StringContent(JsonString, Encoding.UTF8, "application/json"),
+        //        StatusCode = HttpStatusCode.OK
+        //    };
+        //}
 
         // Person må være med i ViewModellen, siden oppdraget er fra en anonym (Ikke registert)
         //[System.Web.Mvc.HttpPost]
